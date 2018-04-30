e0f697b900246a8986f8456fd8580284940ee63e
Gido van Wijk <gwijkvan@jaguarlandrover.com>
2018-04-30 05:05:28 -0700

    Change scope of variables

--- src/logit.c
+++ src/logit.c
@@ -5,9 +5,6 @@
 #include <stdlib.h>
 #include <errno.h>
 
-static char msg_t[255];
-static bool first_call = true;
-
 void _err_msg(char *filename, int line, int errnum, char *sysc)
 {
   fprintf(stderr, "%s:%d: %s: %s\n", filename, line, sysc, strerror(errnum));
@@ -25,25 +22,27 @@ void _log_msg(char *filename, int line, char *fmt, ...)
 
 void _log_msg_r(char *filename, int line, char *fmt, ...)
 {
+  static char msg[255];
+  static bool firstcall = true;
   char tmp[255];
   va_list vargs;
 
-  if (first_call) {
-    first_call = false;
-    strcat(msg_t, filename);
+  if (firstcall) {
+    firstcall = false;
+    strcat(msg, filename);
     sprintf(tmp, ":%d: ", line);
-    strncat(msg_t, tmp, strlen(tmp));
+    strncat(msg, tmp, strlen(tmp));
   }
 
   va_start(vargs, fmt);
   vsprintf(tmp, fmt, vargs);
   va_end(vargs);
-  strncat(msg_t, tmp, strlen(tmp));
+  strncat(msg, tmp, strlen(tmp));
 
   if (strstr(fmt, "\n")) {
-    fprintf(stderr, "%s", msg_t);
-    first_call = true;
-    strncpy(msg_t, "\0", 1);
+    fprintf(stderr, "%s", msg);
+    firstcall = true;
+    strncpy(msg, "\0", 1);
   }
 
 }
